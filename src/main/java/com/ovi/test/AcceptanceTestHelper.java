d992847a766be1dba517c30849db71a18fdcbd72
adutton <devnull@localhost>
2010-12-06 13:46:55 +0000

    Added ability to specified HTTP headers in the HTTP helper methods.

--- src/main/java/com/ovi/test/AcceptanceTestHelper.java
+++ src/main/java/com/ovi/test/AcceptanceTestHelper.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.regex.Pattern;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -37,44 +38,129 @@ import com.ovi.test.matchers.WithSize;
 import com.ovi.test.matchers.WithValueAt;
 
 public class AcceptanceTestHelper {
-
+	private static final Pattern HEADER_PATTERN = Pattern.compile("(.+)[ ]*:[ ]*(.+)"); 
     private static final HttpClient client = new HttpClient();
 
-    public static HttpMethod doGetOf(final Object urlObject) {
+    /**
+     * Executes a HTTP GET request to the URI specified. 
+     * 
+     * @param urlObject
+     * 		The object representing the URL. The toString() method of the object will be used
+     * 		to get a string representation of the URL.
+     * @param headers
+     * 		A list of headers to add to the HTTP request. Each header should be a string in the
+     * 		same form as one would add to the HTTP request itself: e.g. "someheader: somevalue".
+     * @return
+     * 		A handle to the GET {@link HttpMethod} after having been executed.  
+     */
+    public static HttpMethod doGetOf(final Object urlObject, final String... headers) {
         final String url = urlObject.toString();
         
         final HttpMethod method = new GetMethod(url);
+        addHeaders(method, headers);
+        
         return callHttpMethod(method);
     }
 
-    public static HttpMethod doPutOf(final String urlObject, final InputStream requestBody, final String contentType) {
+    /**
+     * Executes a HTTP PUT request to the URI specified. 
+     * 
+     * @param urlObject
+     * 		The object representing the URL. The toString() method of the object will be used
+     * 		to get a string representation of the URL.
+     * @param requestBody
+     * 		The request entity body.
+     * @param contentType
+     * 		The request entity content type. NOTE: this value will be set as the "Content-Type"
+     * 		header for the request ONLY if the "Content-Type" header is not set specifically in the headers
+     * 		parameter. 
+     * @param headers
+     * 		A list of headers to add to the HTTP request. Each header should be a string in the
+     * 		same form as one would add to the HTTP request itself: e.g. "someheader: somevalue".
+     * @return
+     * 		A handle to the PUT {@link HttpMethod} after having been executed.  
+     */
+    public static HttpMethod doPutOf(final String urlObject, final InputStream requestBody, final String contentType, final String... headers) {
         final String url = urlObject.toString();
         
         final PutMethod method = new PutMethod(url);
+        addHeaders(method, headers);
+        
         method.setRequestEntity(new InputStreamRequestEntity(requestBody, contentType));
 
         return callHttpMethod(method);
 
     }
 
-    public static HttpMethod doPutOf(final String urlObject) {
+    /**
+     * Executes a HTTP PUT request to the URI specified. 
+     * 
+     * @param urlObject
+     * 		The object representing the URL. The toString() method of the object will be used
+     * 		to get a string representation of the URL.
+     * @param headers
+     * 		A list of headers to add to the HTTP request. Each header should be a string in the
+     * 		same form as one would add to the HTTP request itself: e.g. "someheader: somevalue".
+     * @return
+     * 		A handle to the PUT {@link HttpMethod} after having been executed.  
+     */
+    public static HttpMethod doPutOf(final String urlObject, final String... headers) {
         final String url = urlObject.toString();
     	
         final PutMethod method = new PutMethod(url);
+        addHeaders(method, headers);        
+        
         return callHttpMethod(method);
 
     }
 
-    public static HttpMethod doPostOf(final String urlObject, final InputStream requestBody, final String contentType) {
+    /**
+     * Executes a HTTP POST request to the URI specified. 
+     * 
+     * @param urlObject
+     * 		The object representing the URL. The toString() method of the object will be used
+     * 		to get a string representation of the URL.
+     * @param requestBody
+     * 		The request entity body.
+     * @param contentType
+     * 		The request entity content type. NOTE: this value will be set as the "Content-Type"
+     * 		header for the request ONLY if the "Content-Type" header is not set specifically in the headers
+     * 		parameter. 
+     * @param headers
+     * 		A list of headers to add to the HTTP request. Each header should be a string in the
+     * 		same form as one would add to the HTTP request itself: e.g. "someheader: somevalue".
+     * @return
+     * 		A handle to the POST {@link HttpMethod} after having been executed.  
+     */
+    public static HttpMethod doPostOf(final String urlObject, final InputStream requestBody, final String contentType, final String... headers) {
         final String url = urlObject.toString();
     	
         final PostMethod method = new PostMethod(url);
+        addHeaders(method, headers);        
+        
         method.setRequestEntity(new InputStreamRequestEntity(requestBody, contentType));
 
         return callHttpMethod(method);
 
     }
 
+    private static void addHeaders(final HttpMethod method, final String[] headers) {
+    	if (headers == null) {
+    		return;
+    	}
+    	
+    	for (String header : headers) {
+        	final java.util.regex.Matcher matcher = HEADER_PATTERN.matcher(header);
+        	
+        	if (matcher.matches()) {
+        		final String name = matcher.group(1);
+        		final String value = matcher.group(2);
+        		
+        		method.setRequestHeader(name, value);
+        	}
+    	}
+    }
+    
     private static HttpMethod callHttpMethod(final HttpMethod method) {
 
         try {
