59f82c59a50fe8011841e53edc2e4ab0d66edd61
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-05-08 01:13:24 +0100

    First cut of Url class.  Needs url-escaping code
    And some thought about which methods should be final etc.

---
+++ rest-server-driver/src/main/java/com/github/restdriver/serverdriver/http/Url.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright © 2010-2011 Nokia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.restdriver.serverdriver.http;
+
+import org.apache.commons.lang.text.StrBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class to help with building of URLs
+ */
+public class Url {
+
+    private StrBuilder url;
+    private List<QueryParam> queryParams;
+
+    private class QueryParam{
+        final String key;
+        final String value;
+
+        private QueryParam(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+    /**
+     * Setup a Url with a base path, like "http://localhost:8080".
+     * @param base the base Url
+     */
+    public Url(String base) {
+        this.url = new StrBuilder( base );
+        queryParams = new ArrayList<QueryParam>();
+    }
+
+    /**
+     * Add a path to a url.  This method will ensure that there is always exactly one "/" character between segments (so you don't have to :).
+     * @param path the path, eg "foo/bar"
+     * @return The new Url object (for chaining calls)
+     */
+    public Url withPath(String path) {
+
+        if ( ! ( url.endsWith("/") || path.startsWith("/") ) ){
+            url.append("/");
+        }
+
+        if ( url.endsWith("/") && path.startsWith("/") ){
+            path = path.substring(1);
+        }
+
+        url.append(path);
+        return this;
+    }
+
+    /**
+     * Adds a query-string parameter to the end of the url, like ?key=val.
+     * @param key The key for the query string.
+     * @param value The value for the query string.
+     * @return The Url with the query string param added (for chaining calls)
+     */
+    public Url withParam(String key, String value) {
+        queryParams.add(new QueryParam(key, value));
+        return this;
+    }
+
+    /**
+     * You can pass this object to all the get/post/put/delete etc methods
+     * @return The Url, correctly formatted.
+     */
+    public String toString() {
+
+        boolean firstParam = true;
+
+        for ( QueryParam qp : queryParams ){
+            if ( firstParam ){
+                url.append("?");   
+                firstParam = false;
+            } else {
+                url.append("&");
+            }
+            url.append(qp.getKey()).append("=").append(qp.getValue());
+        }
+
+        return url.toString();
+    }
+}
---
+++ rest-server-driver/src/test/java/com/github/restdriver/serverdriver/http/UrlTest.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright © 2010-2011 Nokia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.restdriver.serverdriver.http;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * User: mjg
+ * Date: 07/05/11
+ * Time: 23:44
+ */
+public class UrlTest {
+
+    @Test
+    public void basicUrlMakesAString(){
+        String url = new Url("http://localhost").toString();
+        assertThat( url, is("http://localhost") );
+    }
+
+    @Test
+    public void urlWithPathMakesAString(){
+        Url url = new Url("http://localhost").withPath("foo/bar");
+        assertThat( url.toString(), is("http://localhost/foo/bar") );
+    }
+
+    @Test
+    public void urlWithPathMakesAStringWithTrailingSlash(){
+        Url url = new Url("http://localhost/").withPath("foo/bar");
+        assertThat( url.toString(), is("http://localhost/foo/bar") );
+    }
+
+    @Test
+    public void urlWithPathMakesAStringWithLeadingSlash(){
+        Url url = new Url("http://localhost").withPath("/foo/bar");
+        assertThat( url.toString(), is("http://localhost/foo/bar") );
+    }
+
+    @Test
+    public void multiplePathsAreSlashedCorrectly(){
+        Url url = new Url("http://localhost").withPath("a").withPath("/b/").withPath("/c");
+        assertThat( url.toString(), is("http://localhost/a/b/c") );
+    }
+
+    @Test
+    public void endingTrailingSlashIsKept(){
+        Url url = new Url("http://localhost").withPath("a").withPath("/b/").withPath("/c/");
+        assertThat( url.toString(), is("http://localhost/a/b/c/") );
+    }
+
+    @Test
+    public void singleParamIsAdded(){
+        Url url = new Url("http://localhost").withParam("a", "b");
+        assertThat( url.toString(), is("http://localhost?a=b") );
+    }
+
+    @Test
+    public void multipleParamsAreAdded(){
+        Url url = new Url("http://localhost").withParam("a", "b").withParam("c", "d");
+        assertThat( url.toString(), is("http://localhost?a=b&c=d") );
+    }
+
+    @Test
+    public void paramsAndPathsCanBeIntermixed(){
+        // not sure how useful this actually is, but we might as well support it.
+        Url url = new Url("http://localhost").withParam("a", "b").withPath("c/");
+        assertThat( url.toString(), is("http://localhost/c/?a=b") );
+    }
+
+}
