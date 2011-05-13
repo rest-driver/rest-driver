b6d1892cc55f67031819c3538a9d440275b94f3d
Neil Prosser <neil.prosser@gmail.com>
2011-05-13 21:52:07 +0100

    Removing extra directory from client driver tests

--- rest-client-driver/src/test/java/com/github/restdriver/clientdriver/integration/ClientDriverFailTest.java
+++ rest-client-driver/src/test/java/com/github/restdriver/clientdriver/integration/ClientDriverFailTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.restdriver.clientdriver.clientdriver.integration;
+package com.github.restdriver.clientdriver.integration;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
