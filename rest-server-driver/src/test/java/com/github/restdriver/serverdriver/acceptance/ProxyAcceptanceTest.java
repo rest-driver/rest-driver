964f8560a0773a54701ac4eb5d8537fbfeccfb03
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-06-05 12:52:57 +0100

    ctrl-shift-o

--- rest-server-driver/src/test/java/com/github/restdriver/serverdriver/acceptance/ProxyAcceptanceTest.java
+++ rest-server-driver/src/test/java/com/github/restdriver/serverdriver/acceptance/ProxyAcceptanceTest.java
@@ -17,18 +17,11 @@ package com.github.restdriver.serverdriver.acceptance;
 
 import static com.github.restdriver.serverdriver.RestServerDriver.get;
 import static com.github.restdriver.serverdriver.RestServerDriver.usingProxy;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.fail;
-
-import javax.net.ssl.HandshakeCompletedEvent;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.servlets.ProxyServlet;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -37,9 +30,7 @@ import com.github.restdriver.clientdriver.ClientDriver;
 import com.github.restdriver.clientdriver.ClientDriverRequest;
 import com.github.restdriver.clientdriver.ClientDriverResponse;
 import com.github.restdriver.clientdriver.ClientDriverRule;
-import com.github.restdriver.clientdriver.exception.ClientDriverFailedExpectationException;
 import com.github.restdriver.serverdriver.http.exception.RuntimeHttpHostConnectException;
-import com.github.restdriver.serverdriver.http.response.Response;
 
 public class ProxyAcceptanceTest {
 
