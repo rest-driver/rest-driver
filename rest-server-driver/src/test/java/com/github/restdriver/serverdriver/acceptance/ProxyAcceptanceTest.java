6e725afebcef4ae1f227eace75b09adfdf839339
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-06-05 12:52:38 +0100

    rejiggered tests

--- rest-server-driver/src/test/java/com/github/restdriver/serverdriver/acceptance/ProxyAcceptanceTest.java
+++ rest-server-driver/src/test/java/com/github/restdriver/serverdriver/acceptance/ProxyAcceptanceTest.java
@@ -19,6 +19,9 @@ import static com.github.restdriver.serverdriver.RestServerDriver.get;
 import static com.github.restdriver.serverdriver.RestServerDriver.usingProxy;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.fail;
+
+import javax.net.ssl.HandshakeCompletedEvent;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
@@ -28,62 +31,66 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.github.restdriver.clientdriver.ClientDriver;
 import com.github.restdriver.clientdriver.ClientDriverRequest;
 import com.github.restdriver.clientdriver.ClientDriverResponse;
 import com.github.restdriver.clientdriver.ClientDriverRule;
+import com.github.restdriver.clientdriver.exception.ClientDriverFailedExpectationException;
+import com.github.restdriver.serverdriver.http.exception.RuntimeHttpHostConnectException;
 import com.github.restdriver.serverdriver.http.response.Response;
 
 public class ProxyAcceptanceTest {
 
     private int proxyPort;
     private Server proxyServer;
-    
+
     @Rule
     public ClientDriverRule driver = new ClientDriverRule();
 
-    @Before
-    public void startProxy(){
-        proxyPort = startLocalProxy();
-    }
-    
-    @After
-    public void stopProxy(){
-        try {
-            proxyServer.stop();
-        } catch (Exception e) {
-            throw new RuntimeException("Problem stopping Jetty proxy", e);
-        }
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void testWithSpecifiedProxyFailsIfProxyIsNotAvailable() {
+        thrown.expect(RuntimeHttpHostConnectException.class);
+        driver.addExpectation(new ClientDriverRequest("/foo"), new ClientDriverResponse("Content"));
+        get(driver.getBaseUrl() + "/foo", usingProxy("localhost", ClientDriver.getFreePort()));
     }
 
     @Test
-    public void testWithSpecifiedProxy() {
-
+    public void testWithSpecifiedProxyPassesIfProxyIsAvailable() {
+        startLocalProxy();
         driver.addExpectation(new ClientDriverRequest("/foo"), new ClientDriverResponse("Content"));
-
-        Response response = get(driver.getBaseUrl() + "/foo", usingProxy("localhost", proxyPort));
-        assertThat(response.getContent(), is("Content"));
-        
+        get(driver.getBaseUrl() + "/foo", usingProxy("localhost", proxyPort));
+        stopLocalProxy();
     }
 
-    private int startLocalProxy() {
+    private void startLocalProxy() {
         try {
 
             int port = ClientDriver.getFreePort();
 
             proxyServer = new Server(port);
-            ServletContextHandler context = new ServletContextHandler(
-                    ServletContextHandler.SESSIONS);
+            ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
             context.setContextPath("/");
             proxyServer.setHandler(context);
             context.addServlet(new ServletHolder(new ProxyServlet()), "/*");
             proxyServer.start();
 
-            return port;
+            proxyPort = port;
 
         } catch (Exception e) {
             throw new RuntimeException("Proxy setup oops", e);
+        }
+    }
+
+    public void stopLocalProxy() {
+        try {
+            proxyServer.stop();
+        } catch (Exception e) {
+            throw new RuntimeException("Problem stopping Jetty proxy", e);
         }
     }
 
