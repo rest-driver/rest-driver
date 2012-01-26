ac2b5f05f92040daf08c48ae4f3cc7e8105ddd35
Andrew Jones <me@andrewhjon.es>
2012-01-26 16:21:10 +0200

    Add match handlers.
    Add a way to add a match handler. It's useful for debugging to be
    able to do something when a request is matched. For example, if
    a logging URL is called it'd be nice to be able print the body
    of the request.

--- rest-client-driver/src/test/java/com/github/restdriver/clientdriver/integration/ClientDriverRuleTest.java
+++ rest-client-driver/src/test/java/com/github/restdriver/clientdriver/integration/ClientDriverRuleTest.java
@@ -16,10 +16,17 @@
 package com.github.restdriver.clientdriver.integration;
 
 import static com.github.restdriver.clientdriver.RestClientDriver.*;
+import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
+import com.github.restdriver.clientdriver.ClientDriverRequest;
+import com.github.restdriver.clientdriver.HttpRealRequest;
+import com.github.restdriver.clientdriver.MatchedRequestHandler;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.junit.Rule;
 import org.junit.Test;
@@ -29,41 +36,68 @@ import com.github.restdriver.clientdriver.ClientDriverRule;
 import com.github.restdriver.clientdriver.exception.ClientDriverFailedExpectationException;
 
 public class ClientDriverRuleTest {
-    
+
     @Rule
     public ClientDriverRule driver = new ClientDriverRule();
-    
+
     @Rule
     public ExpectedException thrown = ExpectedException.none();
-    
+
     @Test
     public void letsTrySomethingThatFails() throws Exception {
-        
+
         // We use ExpectedException to catch the exception we (hopefully) get because the expectations weren't met
         thrown.expect(ClientDriverFailedExpectationException.class);
-        
+
         driver.addExpectation(onRequestTo("/blah"), giveResponse("OUCH!!").withStatus(200));
         driver.addExpectation(onRequestTo("/blah"), giveResponse("OUCH!!").withStatus(404));
-        
+
         HttpClient client = new DefaultHttpClient();
-        
+
         HttpPost post = new HttpPost(driver.getBaseUrl() + "/blah?gang=groon");
-        
+
         client.execute(post);
-        
+
     }
-    
+
     @Test
     public void letsTrySomethingThatWorks() throws Exception {
-        
+
         driver.addExpectation(onRequestTo("/blah"), giveResponse("").withStatus(404));
-        
+
         HttpClient client = new DefaultHttpClient();
-        
+
         HttpGet get = new HttpGet(driver.getBaseUrl() + "/blah");
-        
+
         client.execute(get);
-        
+
     }
-    
+
+    @Test
+    public void letsTrySomethingThatShouldCallbackOnMatch() throws Exception {
+
+        final boolean[] matched = new boolean[1];
+
+        driver.addExpectation(
+                onRequestTo("/path").withMethod(ClientDriverRequest.Method.POST),
+                giveResponse(""))
+                .anyTimes()
+                .whenMatched(new MatchedRequestHandler() {
+
+                    @Override
+                    public void onMatch(HttpRealRequest matchedRequest) {
+                        assertThat(matchedRequest.getBodyContent(), is("body"));
+                        matched[0] = true;
+                    }
+
+                });
+
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost(driver.getBaseUrl() + "/path");
+        post.setEntity(new StringEntity("body"));
+        client.execute(post);
+
+        assertTrue(matched[0]);
+    }
+
 }
