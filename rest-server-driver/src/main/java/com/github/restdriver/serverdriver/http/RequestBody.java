ff3e29463c4b1bc8c55007d17d6b3e4328823b38
Joe Littlejohn <joe.littlejohn@nokia.com>
2012-08-16 10:24:03 +0100

    Fix parsing of request Content Types that include a charset

--- rest-server-driver/src/main/java/com/github/restdriver/serverdriver/http/RequestBody.java
+++ rest-server-driver/src/main/java/com/github/restdriver/serverdriver/http/RequestBody.java
@@ -15,6 +15,11 @@
  */
 package com.github.restdriver.serverdriver.http;
 
+import static org.apache.commons.lang.StringUtils.*;
+
+import javax.activation.MimeType;
+import javax.activation.MimeTypeParseException;
+
 import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.ContentType;
@@ -24,60 +29,80 @@ import org.apache.http.entity.StringEntity;
  * Encapsulates a Request body for a method.
  */
 public final class RequestBody implements AnyRequestModifier {
-    
+
     private static final String DEFAULT_CONTENT_ENCODING = "UTF-8";
-    
+
     private final String content;
     private final String contentType;
-    
+
     /**
      * Creates a new request body instance.
      * 
-     * @param content A string to use for the content
-     * @param contentType A string representing the content-type
+     * @param content
+     *            A string to use for the content
+     * @param contentType
+     *            A string representing the content-type
      */
     public RequestBody(String content, String contentType) {
         this.content = content;
         this.contentType = contentType;
     }
-    
+
     /**
      * Gets the content of this request body.
      * 
      * @return The content as a string
      * 
-     * @deprecated This shouldn't need to be exposed. Expect it to go away in the future.
+     * @deprecated This shouldn't need to be exposed. Expect it to go away in
+     *             the future.
      */
     @Deprecated
     public String getContent() {
         return content;
     }
-    
+
     /**
      * Gets the content-type of this request body.
      * 
      * @return The content-type as a string
      * 
-     * @deprecated This shouldn't need to be exposed. Expect it to go away in the future.
+     * @deprecated This shouldn't need to be exposed. Expect it to go away in
+     *             the future.
      */
     @Deprecated
     public String getContentType() {
         return contentType;
     }
-    
+
     @Override
     public void applyTo(ServerDriverHttpUriRequest request) {
-        
+
         HttpUriRequest internalRequest = request.getHttpUriRequest();
-        
+
         if (!(internalRequest instanceof HttpEntityEnclosingRequest)) {
             return;
         }
-        
+
         HttpEntityEnclosingRequest entityRequest = (HttpEntityEnclosingRequest) internalRequest;
-        
+
         entityRequest.setHeader("Content-type", contentType);
-        entityRequest.setEntity(new StringEntity(content, ContentType.create(contentType, DEFAULT_CONTENT_ENCODING)));
-        
+        entityRequest.setEntity(new StringEntity(content, createContentType(contentType)));
+
     }
+
+    private ContentType createContentType(String contentType) {
+        try {
+
+            MimeType mimeType = new MimeType(contentType);
+
+            String mediaType = mimeType.getBaseType();
+            String charset = defaultString(mimeType.getParameter("charset"), DEFAULT_CONTENT_ENCODING);
+
+            return ContentType.create(mediaType, charset);
+
+        } catch (MimeTypeParseException e) {
+            throw new IllegalArgumentException("Invalid content type: " + contentType, e);
+        }
+    }
+
 }
