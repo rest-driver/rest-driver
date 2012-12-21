3d3dd3af059752fe37c357c1041e245a05d68914
mjg123 <matthew.gilliard@gmail.com>
2012-12-21 13:47:26 +0000

    Adds an ImmutableUrl

--- rest-server-driver/src/main/java/com/github/restdriver/serverdriver/http/Url.java
+++ rest-server-driver/src/main/java/com/github/restdriver/serverdriver/http/Url.java
@@ -60,12 +60,22 @@ public class Url {
         this.url = new StrBuilder(base);
         queryParams = new ArrayList<QueryParam>();
     }
-    
+
+    /**
+     * Copy constructor.  Creates a deep copy so no parts are shared.
+     *
+     * @param toBeCopied the Url to be copied
+     */
+    public Url(Url toBeCopied){
+        this.url = new StrBuilder(toBeCopied.url.toString());
+        this.queryParams = new ArrayList<QueryParam>(toBeCopied.queryParams);
+    }
+
     /**
      * Add a path to a url. This method ensures that there is always exactly one "/" character between segments (so you don't have to :).
      * 
      * @param path the path, eg "foo/bar"
-     * @return The new Url object (for chaining calls)
+     * @return The modified Url object (for chaining calls)
      */
     public final Url withPath(String path) {
         
@@ -95,8 +105,8 @@ public class Url {
     
     /**
      * You can pass this object to all the get/post/put/delete etc methods.
-     * 
-     * @return The Url, correctly formatted.
+     *
+     * @return The textual representation of the Url, correctly formatted.
      */
     public final String toString() {
         
--- rest-server-driver/src/test/java/com/github/restdriver/serverdriver/http/ImmutableUrlTest.java
+++ rest-server-driver/src/test/java/com/github/restdriver/serverdriver/http/ImmutableUrlTest.java
@@ -20,100 +20,105 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-/**
- * User: mjg
- * Date: 07/05/11
- * Time: 23:44
- */
-public class UrlTest {
-    
+public class ImmutableUrlTest {
+
     @Test
-    public void basicUrlMakesAString() {
-        String url = new Url("http://localhost").toString();
+    public void immutabilityWhenSettingThePath(){
+        ImmutableUrl original = new ImmutableUrl("localhost");
+        ImmutableUrl withPath = original.withPath("path");
+
+        assertThat(original.toString(), is("http://localhost"));
+        assertThat(withPath.toString(), is("http://localhost/path"));
+    }
+
+    @Test
+    public void immutabilityWhenSettingTheParams(){
+        ImmutableUrl original = new ImmutableUrl("localhost");
+        ImmutableUrl withPath = original.withParam("k", "v");
+
+        assertThat(original.toString(), is("http://localhost"));
+        assertThat(withPath.toString(), is("http://localhost?k=v"));
+    }
+
+    // Below are the same tests from UrlTest to validate the behaviour
+
+    @Test
+    public void basicImmutableUrlMakesAString() {
+        String url = new ImmutableUrl("http://localhost").toString();
         assertThat(url, is("http://localhost"));
     }
-    
+
     @Test
     public void urlWithPathMakesAString() {
-        Url url = new Url("http://localhost").withPath("foo/bar");
+        ImmutableUrl url = new ImmutableUrl("http://localhost").withPath("foo/bar");
         assertThat(url.toString(), is("http://localhost/foo/bar"));
     }
-    
+
     @Test
     public void urlWithPathMakesAStringWithTrailingSlash() {
-        Url url = new Url("http://localhost/").withPath("foo/bar");
+        ImmutableUrl url = new ImmutableUrl("http://localhost/").withPath("foo/bar");
         assertThat(url.toString(), is("http://localhost/foo/bar"));
     }
-    
+
     @Test
     public void urlWithPathMakesAStringWithLeadingSlash() {
-        Url url = new Url("http://localhost").withPath("/foo/bar");
+        ImmutableUrl url = new ImmutableUrl("http://localhost").withPath("/foo/bar");
         assertThat(url.toString(), is("http://localhost/foo/bar"));
     }
-    
+
     @Test
     public void multiplePathsAreSlashedCorrectly() {
-        Url url = new Url("http://localhost").withPath("a").withPath("/b/").withPath("/c");
+        ImmutableUrl url = new ImmutableUrl("http://localhost").withPath("a").withPath("/b/").withPath("/c");
         assertThat(url.toString(), is("http://localhost/a/b/c"));
     }
-    
+
     @Test
     public void endingTrailingSlashIsKept() {
-        Url url = new Url("http://localhost").withPath("a").withPath("/b/").withPath("/c/");
+        ImmutableUrl url = new ImmutableUrl("http://localhost").withPath("a").withPath("/b/").withPath("/c/");
         assertThat(url.toString(), is("http://localhost/a/b/c/"));
     }
-    
+
     @Test
     public void singleParamIsAdded() {
-        Url url = new Url("http://localhost").withParam("a", "b");
+        ImmutableUrl url = new ImmutableUrl("http://localhost").withParam("a", "b");
         assertThat(url.toString(), is("http://localhost?a=b"));
     }
-    
+
     @Test
     public void multipleParamsAreAdded() {
-        Url url = new Url("http://localhost").withParam("a", "b").withParam("c", "d");
+        ImmutableUrl url = new ImmutableUrl("http://localhost").withParam("a", "b").withParam("c", "d");
         assertThat(url.toString(), is("http://localhost?a=b&c=d"));
     }
-    
+
     @Test
     public void paramsAndPathsCanBeIntermixed() {
         // not sure how useful this actually is, but we might as well support it.
-        Url url = new Url("http://localhost").withParam("a", "b").withPath("c/");
+        ImmutableUrl url = new ImmutableUrl("http://localhost").withParam("a", "b").withPath("c/");
         assertThat(url.toString(), is("http://localhost/c/?a=b"));
     }
-    
+
     @Test
     public void pathsAreUrlEncoded() {
-        Url url = new Url("http://localhost").withPath("   ");
+        ImmutableUrl url = new ImmutableUrl("http://localhost").withPath("   ");
         assertThat(url.toString(), is("http://localhost/%20%20%20"));
     }
-    
+
     @Test
     public void multiSectionPathsAreUrlEncoded() {
-        Url url = new Url("http://localhost").withPath("   /  ");
+        ImmutableUrl url = new ImmutableUrl("http://localhost").withPath("   /  ");
         assertThat(url.toString(), is("http://localhost/%20%20%20/%20%20"));
     }
-    
+
     @Test
     public void paramsAreUrlEncoded() {
-        Url url = new Url("http://localhost").withParam("a%a", "b>b");
+        ImmutableUrl url = new ImmutableUrl("http://localhost").withParam("a%a", "b>b");
         assertThat(url.toString(), is("http://localhost?a%25a=b%3Eb"));
     }
-    
+
     @Test
     public void assumesHttpAsScheme() {
-        assertThat(new Url("localhost/").toString(), is("http://localhost/"));
+        assertThat(new ImmutableUrl("localhost/").toString(), is("http://localhost/"));
     }
 
-    @Test
-    public void copyConstructor(){
-        Url original = new Url("http://localhost").withPath("x").withPath("y").withParam("a", "b").withParam("c", "d");
-        Url copied = new Url(original);
 
-        copied.withPath("z").withParam("e","f");
-
-        assertThat(original.toString(), is("http://localhost/x/y?a=b&c=d"));
-        assertThat(copied.toString(), is("http://localhost/x/y/z?a=b&c=d&e=f"));
-    }
-    
 }
