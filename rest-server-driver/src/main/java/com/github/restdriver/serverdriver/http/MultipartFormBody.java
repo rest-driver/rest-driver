cf970e01898cd682e797ca25bff58a39263152d9
Cserveny, Tamás <tamas.cserveny@lhsystems.com>
2020-09-07 18:16:57 +0200

    add license, make body handler fluent

--- rest-server-driver/src/main/java/com/github/restdriver/serverdriver/http/MultipartFormBody.java
+++ rest-server-driver/src/main/java/com/github/restdriver/serverdriver/http/MultipartFormBody.java
@@ -1,10 +1,21 @@
 /*
  *  Copyright Lufthansa Systems.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.github.restdriver.serverdriver.http;
 
 import java.net.URLConnection;
-
 
 import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.client.methods.HttpUriRequest;
@@ -19,12 +30,6 @@ import org.apache.http.entity.mime.content.StringBody;
  */
 public class MultipartFormBody implements AnyRequestModifier {
 
-    private static final String LINE_FEED = "\r\n";
-    private static final String DEFAULT_CHARSET = "UTF-8";
-    private String boundary = "xxx" + System.currentTimeMillis() + "xxx";
-
-    private String charset = DEFAULT_CHARSET;
-
     private MultipartEntityBuilder multipartContent = MultipartEntityBuilder.create();
 
     /**
@@ -33,9 +38,10 @@ public class MultipartFormBody implements AnyRequestModifier {
      * @param formField Name of the field
      * @param value String value
      */
-    public void addFormField(String formField, String value) {
+    public MultipartFormBody addFormField(String formField, String value) {
         multipartContent.addPart(FormBodyPartBuilder.create(formField,
                 new StringBody(value, ContentType.TEXT_PLAIN)).build());
+        return this;
     }
 
     /**
@@ -45,12 +51,13 @@ public class MultipartFormBody implements AnyRequestModifier {
      * @param fileName Name of the file
      * @param content byte contents of the file
      */
-    public void addFileField(String formField, String fileName, byte[] content) {
+    public MultipartFormBody addFileField(String formField, String fileName, byte[] content) {
         multipartContent.addPart(FormBodyPartBuilder.create(formField,
                 new ByteArrayBody(content,
                         ContentType.create(URLConnection.guessContentTypeFromName(fileName)),
                         fileName)).
                 build());
+        return this;
     }
 
     @Override
